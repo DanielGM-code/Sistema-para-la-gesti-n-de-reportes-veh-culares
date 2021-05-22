d78da6f1e42259f52bb08f4553721293fcc49dd8
Daniel Morales <62160774+DanielGM-code@users.noreply.github.com>
2021-05-22 17:25:48 -0500

    Primera versión del Login
    Primera versión con cambios estables en el inicio de sesión y hoja de estilos

--- SGRV/Login.xaml.cs
+++ SGRV/Login.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using SGRV.GUIDelegacionesMunicipales;
 using SGRV.GUIDireccionGeneral;
+using SGRV.modelo.dao;
+using SGRV.modelo.database;
+using SGRV.modelo.poco;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,8 +32,64 @@ namespace SGRV
 
         private void button_IniciarSesion_Click(object sender, RoutedEventArgs e)
         {
-            MenuDelegacionMunicipal menu = new MenuDelegacionMunicipal();
-            menu.Show();
+            if (validadCampos())
+            {
+                String username = tb_username.Text;
+                String contraseña = pb_password.Password;
+
+                Usuario usuario = UsuarioDAO.getUsuarioByUsername(username);
+                if (usuario.IdUsuario != 0)
+                {
+                    if (usuario.Contraseña == contraseña)
+                    {
+                        switch (usuario.Cargo)
+                        {
+                            case "Soporte":
+                            case "Administrativo":
+                                MenuDireccionGeneral menuDireccionGeneral = new MenuDireccionGeneral();
+                                menuDireccionGeneral.Show();
+                                this.Close();
+                                break;
+                            case "AgenteDeTransito":
+                            case "Perito":
+                                MenuDelegacionMunicipal menuDelegacionMunicipal = new MenuDelegacionMunicipal();
+                                menuDelegacionMunicipal.Show();
+                                this.Close();
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Contraseña incorrecta.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Usuario no encontrado");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Asegúrese de llenar todos los campos.");
+            }
+            
+        }
+
+        private bool validadCampos()
+        {
+            return (tb_username.Text == "" || pb_password.Password == "") ? false : true;
+        }
+
+        private void Login_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed) {
+                DragMove();
+            }
+        }
+
+        private void button_cerrarVentana_Click(object sender, RoutedEventArgs e)
+        {
             this.Close();
         }
     }
