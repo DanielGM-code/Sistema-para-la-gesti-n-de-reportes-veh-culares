d85f940532350c3a83879aec3de20fcd1f20da16
ANGEL DE JESUS SANCHEZ MORALES <zS18012154@estudiantes.uv.mx>
2021-05-22 18:23:53 -0500

    Borrar Prueba

--- SGRV/obj/Debug/net5.0-windows/GUIDireccionGeneral/RegistrarDelegacionMunicipal.g.i.cs
+++ SGRV/obj/Debug/net5.0-windows/GUIDireccionGeneral/RegistrarDelegacionMunicipal.g.i.cs
@@ -95,13 +95,13 @@ namespace SGRV.GUIDireccionGeneral {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.4.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/SGRV;component/guidirecciongeneral/registrardelegacionmunicipal.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SGRV;V1.0.0.0;component/guidirecciongeneral/registrardelegacionmunicipal.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\GUIDireccionGeneral\RegistrarDelegacionMunicipal.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -111,7 +111,7 @@ namespace SGRV.GUIDireccionGeneral {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.6.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.4.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
