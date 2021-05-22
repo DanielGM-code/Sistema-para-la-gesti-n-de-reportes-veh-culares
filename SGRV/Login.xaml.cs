4e12cb5c0f91f99caaf2b1f1d1d3f5c1bd31ff2c
Angel de Jesús Sánchez Morales <62160767+AngelTheTwin@users.noreply.github.com>
2021-05-22 15:41:39 -0500

    Login Terminado

--- SGRV/Login.xaml.cs
+++ SGRV/Login.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using SGRV.GUIDelegacionesMunicipales;
 using SGRV.GUIDireccionGeneral;
+using SGRV.modelo.dao;
+using SGRV.modelo.database;
+using SGRV.modelo.poco;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,9 +32,53 @@ namespace SGRV
 
         private void button_IniciarSesion_Click(object sender, RoutedEventArgs e)
         {
-            MenuDelegacionMunicipal menu = new MenuDelegacionMunicipal();
-            menu.Show();
-            this.Close();
+            if (validadCampos())
+            {
+                String username = tb_username.Text;
+                String contraseña = pb_contraseña.Password;
+
+                Usuario usuario = UsuarioDAO.getUsuarioByUsername(username);
+                if (usuario.IdUsuario != 0)
+                {
+                    if (usuario.Contraseña == contraseña)
+                    {
+                        switch (usuario.Cargo)
+                        {
+                            case "Soporte":
+                            case "Administrativo":
+                                MenuDireccionGeneral menuDireccionGeneral = new MenuDireccionGeneral();
+                                menuDireccionGeneral.Show();
+                                this.Close();
+                                break;
+                            case "AgenteDeTransito":
+                            case "Perito":
+                                MenuDelegacionMunicipal menuDelegacionMunicipal = new MenuDelegacionMunicipal();
+                                menuDelegacionMunicipal.Show();
+                                this.Close();
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Contraseña incorrecta.");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Usuario no encontrado");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Asegúrese de llenar todos los campos.");
+            }
+            
+        }
+
+        private bool validadCampos()
+        {
+            return (tb_username.Text == "" || pb_contraseña.Password == "") ? false : true;
         }
     }
 }
