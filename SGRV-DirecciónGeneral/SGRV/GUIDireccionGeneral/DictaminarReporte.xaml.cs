4688a0dabf3fce119a207a7c7322fc983167d2ea
Daniel Morales <62160774+DanielGM-code@users.noreply.github.com>
2021-06-11 03:28:09 -0500

    Add files

--- SGRV-DirecciónGeneral/SGRV/GUIDireccionGeneral/DictaminarReporte.xaml.cs
+++ SGRV-DirecciónGeneral/SGRV/GUIDireccionGeneral/DictaminarReporte.xaml.cs
@@ -1,4 +1,5 @@
-﻿using SGRV.modelo.poco;
+﻿using SGRV.modelo.dao;
+using SGRV.modelo.poco;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,15 +21,54 @@ namespace DireccionGeneral.GUIDireccionGeneral
     /// </summary>
     public partial class DictaminarReporte : Window
     {
+        List<Perito> peritos;
+        Perito peritoSeleccionado;
+
         public int idReporte { get; set; }
 
         public DictaminarReporte(int idReporte)
         {
             InitializeComponent();
+
+            peritos = PeritoDAO.getAllPeritos();
+            cb_perito.ItemsSource = peritos;
+
+            tb_Folio.Text = idReporte.ToString();
         }
 
         private void button_RegistrarDictamen_Click(object sender, RoutedEventArgs e)
         {
+            if (validarCampos())
+            {
+                Dictamen dictamen = new Dictamen();
+                dictamen.Descripcion = tb_Descripcion.Text;
+                peritoSeleccionado = (Perito)cb_perito.SelectedItem;
+                dictamen.IdPerito = peritoSeleccionado.IdPerito;
+                dictamen.Estado = "Inactivo";
+                string hora = DateTime.Now.ToString("t");
+                dictamen.Hora = hora;
+
+                try
+                {
+                    DictamenDAO.updateDictamen(dictamen);
+                    MessageBox.Show("Dictamen realizado de manera exitosa.");
+                    this.Close();
+                }
+                catch (Exception x)
+                {
+                    MessageBox.Show("Ocurrió un error, inténtelo de nuevo.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Asegúrese de llenar todos los campos.");
+            }
+        }
+
+        private bool validarCampos()
+        {
+            return (tb_Descripcion.Text == "" ||
+                    cb_perito.SelectedItem == null) ? false : true;
         }
 
         private void button_MinimizarVentana_Click(object sender, RoutedEventArgs e)
